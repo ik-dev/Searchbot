ed3a09d8a8d7ffa8baceddebead5eb0a843de45b
Ibad Khan <ik.ibadkhan@gmail.com>
2016-03-09 21:43:20 -0800

    Adding capability to list pages on hypemachine

--- hypem_crawler.py
+++ hypem_crawler.py
@@ -11,11 +11,15 @@ def get_args():
                         type=str,
                         default='popular',
                         help='Genre type to search for.')
-    parser.add_argument('-p', '--pages',
+    parser.add_argument('-n', '--number',
                         action='store',
                         type=int,
                         default=1,
                         help='Number of pages to list.')
+    parser.add_argument('-p', '--proxy',
+                        action='store',
+                        type=str,
+                        help='Proxy to be used while contacting HypeMachine')
     return parser.parse_args()
 
 
@@ -23,7 +27,7 @@ args = get_args()
 
 genre = '+'.join(args.genre.lower().split())
 
-pages = args.pages or 1
+pages = args.number or 1
 
 ts = int(time.time())
 
@@ -32,6 +36,12 @@ if 'popular' in genre:
 else:
     base_url = 'http://hypem.com/tags/%s' % genre
 
+if args.proxy:
+    proxy = {'http': args.proxy, 'https': args.proxy}
+    proxy_handler = urllib2.ProxyHandler(proxy)
+    opener = urllib2.build_opener(proxy_handler)
+    urllib2.install_opener(opener)
+
 for page in xrange(1, pages + 1):
     url = base_url + '/%d/?ax=1&ts=%d' % (page, ts)
     source = urllib2.urlopen(url).read()
@@ -39,4 +49,4 @@ for page in xrange(1, pages + 1):
     for track in tree.findAll('h3', {'class': 'track_name'}):
         track_artist = track.find('a', {'class': 'artist'}).text.title()
         track_name = track.find('a', {'class': 'track'}).text.title()
-        print '%s %s' % (track_artist.strip(), track_name.strip())
+        print '%s - %s' % (track_artist.strip(), track_name.strip())
